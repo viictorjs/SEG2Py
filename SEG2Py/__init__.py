681448c625c863539b84c26593ece95094a194a3
viictorjs <vjs279@hotmail.com>
2015-10-04 21:01:47 -0300

    SEG2Py

--- SEG2Py/__init__.py
+++ SEG2Py/__init__.py
@@ -1,8 +1,8 @@
 
 '''
 
-    Seg2Py is a Python 3 module which offers a flexible and easy way to visualize
-    SEG2 waveform data using the ObsPy framework.
+    SEG2Py is a Python 3 module that works around the ObsPy framework offering a flexible and
+    easy way to visualize SEG2 seismic sections.
     
     Author: Victor Guedes (Geophysics undergraduate stundent at University of Brasilia - Brazil)
     E-mail: vjs279@hotmail.com
@@ -14,29 +14,54 @@ import matplotlib.pyplot as plt
 def plotSEG2(obspy_stream, *args, **kwargs):
 
     # default:
-    amplitude_gain = 1
-    amplitude_shading = False
+    normalized = True
+    gain = 1
+    shading = False
     invertY = False
     clip = False
+    record_start = 0
+    record_end = obspy_stream[0].stats.delta*len(obspy_stream[0])*1000
+    parameter_error = None
     
     for i in kwargs:
 
-        if i == 'gain':
+        if i == 'normalized':
 
-            if type(kwargs[i]) == int or type(kwargs[i]) == float:
-            
-                amplitude_gain = kwargs['gain']
+            if kwargs[i] == True:
+
+                pass
+
+            elif kwargs[i] == False:
+
+                normalized = False
 
             else:
 
-                print('\nSeg2Py error: the gain parameter should be an intenger or a float.')
-                amplitude_gain = 'error'                
+                print('\SEG2Py ERROR: THE normalized PARAMETER SHOULD BE TRUE OR FALSE.')
+                parameter_error = True 
+
+        elif i == 'gain':
+
+            if type(kwargs[i]) == int or type(kwargs[i]) == float:
+
+                if kwargs[i] > 0:
+            
+                    gain = kwargs['gain']
+
+                else:
+
+                    print('\nSEG2Py ERROR: THE gain PARAMETER SHOULD BE GREATER THAN 0.')
+                    parameter_error = True     
+            else:
+
+                print('\nSEG2Py ERROR: THE gain PARAMETER SHOULD BE AN INTEGER OR A FLOAT.')
+                parameter_error = True              
 
         elif i == 'shading':
 
             if kwargs[i] == True:
 
-                amplitude_shading = True
+                shading = True
 
             elif kwargs[i] == False:
 
@@ -44,8 +69,8 @@ def plotSEG2(obspy_stream, *args, **kwargs):
 
             else:
 
-                print('\nSeg2Py error: the shading parameter should be True or False.')
-                amplitude_shading = 'error'
+                print('\SEG2Py ERROR: THE shading PARAMETER SHOULD BE TRUE OR FALSE.')
+                parameter_error = True 
 
         elif i == 'clip':
 
@@ -59,8 +84,8 @@ def plotSEG2(obspy_stream, *args, **kwargs):
 
             else:
 
-                print('\nSeg2Py error: the clip parameter should be True or False.')
-                clip = 'error'
+                print('\nSEG2Py ERROR: THE clip PARAMETER SHOULD BE TRUE OR FALSE.')
+                parameter_error = True 
 
         elif i == 'invertY':
 
@@ -74,10 +99,51 @@ def plotSEG2(obspy_stream, *args, **kwargs):
 
             else:
 
-                print('\nSeg2Py error: the invertY parameter should be True or False.')
-                invertY = 'error'
+                print('\nSEG2Py ERROR: THE invertY PARAMETER SHOULD BE TRUE OR FALSE.')
+                parameter_error = True
+
+        elif i == 'record_start':
+
+            if type(kwargs[i]) == int or type(kwargs[i]) == float:
+
+                if kwargs[i] < record_end:
+            
+                    record_start = kwargs[i]
+
+                else:
+                    
+                    print('\nSEG2Py ERROR: THE record_start PARAMETER SHOULD BE LESS THAN %.1f ms.'%record_end)
+                    parameter_error = True 
+
+            else:
+
+                print('\nSEG2Py ERROR: THE record_start PARAMETER SHOULD BE AN INTEGER OR A FLOAT.')
+                parameter_error = True
+
+        elif i == 'record_end':
+
+            if type(kwargs[i]) == int or type(kwargs[i]) == float:
+
+                if kwargs[i] > 0 and kwargs[i] > record_start:
+            
+                    record_end = kwargs[i]
+
+                else:
+                    
+                    print('\nSEG2Py ERROR: THE record_end PARAMETER SHOULD BE GREATER THAN %.1f ms.'%record_start)
+                    parameter_error = True 
+
+            else:
+
+                print('\nSEG2Py ERROR: THE record_end PARAMETER SHOULD BE AN INTEGER OR A FLOAT.')
+                parameter_error = True
+
+        else:
+
+            print('\nSEG2Py ERROR: %s IS NOT A SEG2Py RECOGNIZED PARAMETER.'%str(i))
+            parameter_error = True 
         
-    if amplitude_gain != 'error' and amplitude_shading != 'error' and invertY != 'error' and clip != 'error':
+    if parameter_error == None:
 
         if obspy_stream[0].stats._format == 'SEG2':
             
@@ -85,11 +151,19 @@ def plotSEG2(obspy_stream, *args, **kwargs):
             geophones_dx = float(obspy_stream[1].stats.seg2['RECEIVER_LOCATION'])-float(obspy_stream[0].stats.seg2['RECEIVER_LOCATION'])
             fig = plt.figure()
             ax = fig.add_subplot(111)
+            st_max = max([max(obspy_stream[i]) for i in range(len(obspy_stream))])
 
             for i in range(len(obspy_stream)):
+
+                if normalized == True:
                 
-                trace, = ax.plot((obspy_stream[i].data/obspy_stream[i].data.max())*(-1)*amplitude_gain+pos_geophone1+geophones_dx*i,
-                            [obspy_stream[i].stats.delta*k*1000 for k in range(len(obspy_stream[i]))], color='black')
+                    trace, = ax.plot((obspy_stream[i].data/max(obspy_stream[i].data))*(-1)*gain+pos_geophone1+geophones_dx*i,
+                                [obspy_stream[i].stats.delta*k*1000 for k in range(len(obspy_stream[i]))], color='black')
+
+                else:
+                
+                    trace, = ax.plot((obspy_stream[i].data/st_max)*(-1)*gain+pos_geophone1+geophones_dx*i,
+                                [obspy_stream[i].stats.delta*k*1000 for k in range(len(obspy_stream[i]))], color='black')    
 
                 if clip == True:
 
@@ -97,14 +171,16 @@ def plotSEG2(obspy_stream, *args, **kwargs):
                     trace.get_xdata()[trace.get_xdata() > pos_geophone1+i*geophones_dx+((geophones_dx/2)*0.9)] = pos_geophone1+i*geophones_dx+((geophones_dx/2)*0.9)
                     trace.set_xdata(trace.get_xdata())
 
-                if amplitude_shading == True:
+                if shading == True:
 
                     somb = ax.fill_betweenx(trace.get_ydata(),pos_geophone1+geophones_dx*i,trace.get_xdata(),
                                         where = trace.get_xdata() >= pos_geophone1+i*geophones_dx,color='black')
 
             plt.title('Source: %s m | %d channels'%(obspy_stream[0].stats.seg2['SOURCE_LOCATION'],len(obspy_stream)))
+            fig.canvas.set_window_title('SEG2Py - Seismic section')
             plt.xlabel('Distance (m)')
             plt.ylabel('Time (ms)')
+            plt.ylim(record_start,record_end)
             plt.xlim(pos_geophone1-geophones_dx,pos_geophone1+geophones_dx*len(obspy_stream))
 
             if invertY == True:
@@ -115,6 +191,5 @@ def plotSEG2(obspy_stream, *args, **kwargs):
 
         else:
 
-            print("\nSeg2Py error: the obspy_stream you are using is not in seg2 format.")
+            print("\nSEG2Py ERROR: THE OBSPY STREAM YOU'RE USING IS NOT IN SEG2 FORMAT.")
                 
-
